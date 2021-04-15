0b1f147d4ad385983c00e13df8b380c0e03744f2
yohan-kuncoro <odoo@aspd1.odoo.com>
2021-04-15 06:02:32 +0000

    Update API for AP

--- api_sbt/controllers/controllers.py
+++ api_sbt/controllers/controllers.py
@@ -22,7 +22,6 @@ class ApiSbt(http.Controller):
 #             'object': obj
 #         })
 
-class BoonApiRequest(http.Controller):
     @http.route('/api/partner', type='json', auth='public', methods=['GET'])
     def get_partner(self, id):
         sales_rec = request.env['res.partner'].search([])
@@ -44,37 +43,54 @@ class BoonApiRequest(http.Controller):
         
         created = 0
         error = {}
-        error_cnt = 1
+        warn_cnt = 1
         bill_lines = []
         temp_vendor = 0
+        is_error = False
+        response_msg = ""
         
         for rec in ap:
+            temp_state = 0
             
             #Vendor
             partner = request.env['res.partner'].search([('x_studio_subcon_code', '=', rec['subconCode'])])
             if partner['x_studio_subcon_code'] == rec['subconCode']:
                 temp_vendor = partner['id']
             else:
-                request.env['res.partner'].create({
+                #State
+                state = request.env['res.country.state'].search([('name', '=', rec['subconState'])])
+                if state['name'] == rec['subconState']:
+                    temp_state = state['id']
+                else:
+                    error["Error"] = "State  " + rec['subconState'] + " not found"
+                    is_error = True
+                    break
+                
+                created_vendor = request.env['res.partner'].create({
                     "x_studio_subcon_code": rec['subconCode'],
                     "name": rec['subconName'],
                     "street": rec['subconAddress'],
                     "city": rec['subconCity'],
-                    #"state_id": rec['subconState'],
+                    "state_id": temp_state,
                     "zip": rec['subconZipCode'],
                     #"country_id": rec['subconCountry'],
                     "mobile": rec['subconMobileNo'],
                     "email": rec['subconEmail'],
                     #"property_supplier_payment_term_id": rec['subconPaymentTerm']
                     "is_company": 'TRUE',
-                    "supplier_rank": 1
+                    "supplier_rank": 1,
+                    "property_account_receivable_id": 558,
+                    "property_account_payable_id": 606
                 })
                 
-                err_cnt = "Error " + str(error_cnt)
-                error[err_cnt] = "Vendor  " + rec['subconCode'] + " has been created"
-                error_cnt += 1
+                warn_str = "Warning " + str(warn_cnt)
+                error[warn_str] = "Vendor  " + rec['subconCode'] + " has been created"
+                warn_cnt += 1
                 
-                temp_vendor = request.env['res.partner'].search([('x_studio_subcon_code', '=', rec['subconCode'])]).id
+                temp_vendor = created_vendor['id']
+            
+            if is_error == True:
+                break
             
             #Bill Line
             for line in rec['apLine']:
@@ -87,23 +103,26 @@ class BoonApiRequest(http.Controller):
                 if product['default_code'] == line['paymentChargeCode']:
                     temp_product = product['id']
                 else:
-                    request.env['product.product'].create({
+                    created_product = request.env['product.product'].create({
                         "default_code": line['paymentChargeCode'],
-                        "name": line['paymentChargeCodeDesc']
+                        "name": line['paymentChargeCodeDesc'],
+                        "property_account_income_id": line['glCode'],
+                        "property_account_expense_id": line['glCode']
                     })
                     
-                    err_cnt = "Error " + str(error_cnt)
-                    error[err_cnt] = "Product  " + line['paymentChargeCode'] + " has been created"
-                    error_cnt += 1
+                    temp_product = created_product['id']
+                    
+                    warn_str = "Warning " + str(warn_cnt)
+                    error[warn_str] = "Product  " + line['paymentChargeCode'] + " has been created"
+                    warn_cnt += 1
                 
                 #G/L Account
                 account = request.env['account.account'].search([('code', '=', line['glCode'])])
                 if account['code'] == line['glCode']:
                     temp_account = account['id']
                 else:
-                    err_cnt = "Error " + str(error_cnt)
-                    error[err_cnt] = "Account  " + line['glCode'] + " not found"
-                    error_cnt += 1
+                    error["Error"] = "Account  " + line['glCode'] + " not found"
+                    is_error = True
                     break
                 
                 #Tax
@@ -112,9 +131,8 @@ class BoonApiRequest(http.Controller):
                 if tax['amount'] == tx:
                     temp_tax.append(tax['id'])
                 else:
-                    err_cnt = "Error " + str(error_cnt)
-                    error[err_cnt] = "Tax  " + line['paymentTaxRate'] + " not found"
-                    error_cnt += 1
+                    error["Error"] = "Tax  " + line['paymentTaxRate'] + " not found"
+                    is_error = True
                     break
                 
                 order_date = datetime.datetime.strptime(line['omsOrderDate'], '%d/%m/%Y')
@@ -135,6 +153,9 @@ class BoonApiRequest(http.Controller):
                 bill_line['x_vehicle_number'] = line['vehicleId']
                 
                 bill_lines.append(bill_line)
+            
+            if is_error == True:
+                break
                
             payment_date = datetime.datetime.strptime(rec["omsPaymentDate"], '%d/%m/%Y')
             invoice_date = datetime.datetime.strptime(rec["omsPayOpdate1"], '%d/%m/%Y')
@@ -152,6 +173,13 @@ class BoonApiRequest(http.Controller):
                 "invoice_line_ids": bill_lines
             })
             created += 1
+            
+            response_msg = "Bill ID: " + str(result['id'])
         
-        message = {'status': 200, 'response': result, 'error': error, 'message': str(created) + ' record(s) created successfully'}
+        message = {
+            'status': 200, 
+            'response': response_msg, 
+            'error': error, 
+            'message': str(created) + ' record(s) created successfully'
+        }
         return message
